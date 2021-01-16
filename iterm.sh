5c33486231e4a74bb2d033791effe91d828594ff
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-01-16 22:41:53 +0900

    make execute script by argument

--- iterm.sh
+++ iterm.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
+main() {
+  cd `dirname $0`
 
-cd `dirname $0`
+  if [ -n "$1" ]; then
+    eval "./scripts/$1"
+    return
+  fi
 
-action=$(ls -1 ./scripts | fzf)
-[ -z "$action" ] && return
-eval "./scripts/${action}"
+  action=$(ls -1 ./scripts | fzf)
+  [ -z "$action" ] && return
+  eval "./scripts/${action}"
+}
 
-
+main $@
